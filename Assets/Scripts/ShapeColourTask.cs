d7082ed9ac2a58c277122ad08a244ddca427b889
Elliot Millington <elliot.millington1@gmail.com>
2021-06-15 11:57:06 +0100

    Added infrastructure for random shapes and colours

--- Assets/Scripts/ShapeColourTask.cs
+++ Assets/Scripts/ShapeColourTask.cs
@@ -15,6 +15,18 @@ namespace SCE
         //Array of potential colours
         public Material[] possibleColours;
 
+        //Array of target objects
+        private List<GameObject> targets;
+
+        //Array of shape option objects
+        private List<GameObject> shapeOptions;
+
+        private void Start()
+        {
+            targets = GameObject.Find("Stand").GetChildren();
+            shapeOptions = GameObject.Find("Board").GetChildren();
+        }
+
         public void GenerateExperiment(Session session)
         {
             //Create a new block
@@ -22,11 +34,15 @@ namespace SCE
 
             foreach (Trial trial in newBlock.trials)
             {
+                //Randomising shape and colour option
+                foreach (GameObject go in shapeOptions)
+                {
+                    go.GetComponent<MeshFilter>().mesh = possibleShapes[Random.Range(0, possibleShapes.Length - 1)];
+                    go.GetComponent<Renderer>().material = possibleColours[Random.Range(0, possibleColours.Length - 1)];
+                }
                 //TODO Randomise which shapes and colours are visible
                 //TODO Randomise their orientation?
                 //TODO Randomise which of these are selected as the key shapes
-                possibleShapes[0] = null;
-                possibleColours[0] = null;
             }
         }
     }
