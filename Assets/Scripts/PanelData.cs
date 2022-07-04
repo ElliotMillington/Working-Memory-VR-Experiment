43b71e392967f8e7837bc8ff1e0cd728a25def78
AndrewParker770 <andrewparker767@yahoo.co.uk>
2022-07-05 00:17:29 +0100

    panel data progression

---
+++ Assets/Scripts/PanelData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PanelData
+{
+    public int dimension;
+    public int targetNum;
+    public int optionNum;
+
+    public string optionDistro;
+    public float delay_time;
+
+    //selected colours
+    //selected shapes
+
+    public PanelData (PanelObject panel)
+    {
+
+    }
+
+}
