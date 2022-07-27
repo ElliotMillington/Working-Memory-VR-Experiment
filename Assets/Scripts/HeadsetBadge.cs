a1e7e7a311135b4dfc82ae69936379e4788278f6
AndrewParker770 <andrewparker767@yahoo.co.uk>
2022-07-27 01:02:32 +0100

    VR can be initialised manually

--- Assets/Scripts/HeadsetBadge.cs
+++ Assets/Scripts/HeadsetBadge.cs
@@ -1,80 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.Management;
+using System.Collections;
 
 namespace WorkingMemory
 {
     
     public class HeadsetBadge : MonoBehaviour
     {
-
-        private bool isPulsing;
-        private bool isIncreasing;
-
         private bool isPaused;
 
         public Text questionMark;
         public Text warningText;
 
-
-        private float startWidth;
-        private float startHeight;
-
-        private float maxContraction = 10f; 
-
-        private float expansionRate = 0.25f;
-
         [HideInInspector]
         public PanelGroup script;
+
+        public GameObject VRBadge;
+        public GameObject ErrorMessage;
         
         private void Start() {
-            isPulsing = true;
-            isPaused = false;
-            startWidth = this.gameObject.GetComponent<RectTransform>().rect.width;
-            startHeight = this.gameObject.GetComponent<RectTransform>().rect.height;
-
             script = GameObject.FindGameObjectWithTag("panelGroup").GetComponent<PanelGroup>();
-        }
-
-        
-
-        void Update()
-        {
-            if (isPulsing && !isPaused)
-            {
-                if (isIncreasing)
-                {
-                    if (this.gameObject.GetComponent<RectTransform>().rect.width > startWidth + maxContraction)
-                    {
-                        isIncreasing = false;
-                    }else{
-                        this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(this.gameObject.GetComponent<RectTransform>().rect.width + expansionRate, this.gameObject.GetComponent<RectTransform>().rect.height + expansionRate);
-                    }
-                }else{
-                    if (this.gameObject.GetComponent<RectTransform>().rect.width < startWidth - maxContraction)
-                    {
-                        isIncreasing = true;
-                    }else{
-                        this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(this.gameObject.GetComponent<RectTransform>().rect.width - expansionRate, this.gameObject.GetComponent<RectTransform>().rect.height - expansionRate);
-                    }
-                }
-            }
 
             if(script.headsetActive)
             {
-                this.gameObject.SetActive(false);
+                VRBadge.SetActive(false);
             }else{
-                this.gameObject.SetActive(true);
+                VRBadge.SetActive(true);
             }
         }
 
 
         public void badgeMouseOver()
         {
-            isPulsing = false;
-            this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(startWidth, startHeight);
-
             questionMark.gameObject.SetActive(true);
             warningText.gameObject.SetActive(false);
         }
@@ -85,16 +43,38 @@ namespace WorkingMemory
             warningText.gameObject.SetActive(true);
         }
 
-        public void setPaused(bool value)
-        {
-            isPaused = value;
-        }
-
         public void endApplication()
         {
             Application.Quit();
         }
         
-    }
 
+        public void initialiseVR()
+        {
+            XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+
+            if (!XRGeneralSettings.Instance.Manager.isInitializationComplete)
+            {
+                StartCoroutine(showVRErrorText());
+            }else{
+                script.headsetActive = true;
+            }
+        }
+
+        public IEnumerator showVRErrorText()
+        {
+            ErrorMessage.SetActive(true);
+            VRBadge.SetActive(false);
+
+            yield return new WaitForSeconds(5f);
+
+            ErrorMessage.SetActive(false);
+            VRBadge.SetActive(true);
+            badgeMouseExit();
+        }
+
+
+
+    }
 }
